720970005b0abdab513c808d5c97b086aa83f7ba
bochkovyi <bochkovyi@gmail.com>
2018-03-18 09:22:17 +0200

    Cleanup

---
+++ src/components/common/Ship.test.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { Ship } from './Ship';
+import { Grid, Props as BoardProps } from '../Grid/Grid';
+
+const boardProps: BoardProps = {
+    gameOver: false,
+    gameStarted: true,
+    boardUntoched: true,
+    boardSize: 10,
+    ships: [new Ship(10, 5, true)],
+    clickHandler: (e) => console.log(e)
+}
+it('renders Board with a Ship without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<Grid {...boardProps}/>, div);
+});
